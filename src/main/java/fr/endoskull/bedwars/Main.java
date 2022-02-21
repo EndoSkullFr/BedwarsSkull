b4cb502c5b2a432245c547ae91a724265eed46bf
MrKing.exe <sbai.siphayes5@gmail.com>
2022-02-21 15:40:31 +0100

    Patch Day cycle and weather cycle and set time to 0

--- src/main/java/fr/endoskull/bedwars/Main.java
+++ src/main/java/fr/endoskull/bedwars/Main.java
@@ -1,6 +1,7 @@
 package fr.endoskull.bedwars;
 
 import fr.endoskull.bedwars.listeners.StartListener;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
@@ -11,6 +12,9 @@ public class Main extends JavaPlugin {
         instance = this;
         getServer().getPluginManager().registerEvents(new StartListener(), this);
         super.onEnable();
+        Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), "gamerule doDaylightCycle false");
+        Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), "time set 0");
+        Bukkit.dispatchCommand(Bukkit.getServer().getConsoleSender(), "gamerule doWeatherCycle false");
     }
 
     public static Main getInstance() {
