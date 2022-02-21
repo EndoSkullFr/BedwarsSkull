eb8ecd314cae61cf7f22e433b75d5214e6a04668
MrKing.exe <sbai.siphayes5@gmail.com>
2022-02-21 14:47:03 +0100

    onDeadOnVoid

--- src/main/java/fr/endoskull/bedwars/listeners/StartListener.java
+++ src/main/java/fr/endoskull/bedwars/listeners/StartListener.java
@@ -4,12 +4,14 @@ import fr.endoskull.bedwars.Main;
 import fr.endoskull.bedwars.utils.ActionBar;
 import net.md_5.bungee.api.ChatMessageType;
 import net.minecraft.server.v1_8_R3.ChatMessage;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
+import org.bukkit.*;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
@@ -35,6 +37,16 @@ public class StartListener implements Listener {
             int getAllPlayers = Main.getInstance().getServer().getOnlinePlayers().size();
             ActionBar actionBar = new ActionBar(ChatColor.GREEN + "En attente de joueurs" + "  " + getAllPlayers + "/12");
             actionBar.sendToAll();
+        }
+    }
+
+    @EventHandler
+    public void onDeadOnVoid(EntityDamageEvent e) {
+        if(e.getEntity().getType().equals(EntityType.PLAYER)) {
+            if(e.getCause().equals(EntityDamageEvent.DamageCause.VOID)) {
+                Entity entity = e.getEntity();
+                entity.teleport(new Location(e.getEntity().getWorld(), 1345.655, 24, -148));
+            }
         }
     }
 }
