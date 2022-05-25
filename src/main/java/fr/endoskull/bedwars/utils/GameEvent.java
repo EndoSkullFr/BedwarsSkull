60dd4e72ed7c220805d5b52483dc13f8ed2300bf
BebeDlaStreat <bebe.vandroux@gmail.com>
2022-05-25 21:11:04 +0200

    change game events

--- src/main/java/fr/endoskull/bedwars/utils/GameEvent.java
+++ src/main/java/fr/endoskull/bedwars/utils/GameEvent.java
@@ -1,18 +1,18 @@
 package fr.endoskull.bedwars.utils;
 
 public enum GameEvent {
-    /*diamond2(360, "emerald2"),
+    diamond2(360, "emerald2"),
     emerald2(360, "diamond3"),
     diamond3(360, "emerald3"),
     emerald3(360, "bedDestroy"),
     bedDestroy(360, "gameOver"),
-    gameOver(600, "none");*/
-    diamond2(1, "emerald2"),
+    gameOver(600, "none");
+    /*diamond2(1, "emerald2"),
     emerald2(1, "diamond3"),
     diamond3(1, "emerald3"),
     emerald3(1, "bedDestroy"),
     bedDestroy(3, "gameOver"),
-    gameOver(5, "none");
+    gameOver(5, "none")*/
 
     GameEvent(int duration, String next) {
         this.duration = duration;
